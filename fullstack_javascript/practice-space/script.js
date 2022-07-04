36af39514d5428c531e4084ed3633854246b9498
Claudio Camba <claudio.a.jose@gmail.com>
2022-07-04 21:22:14 +0100

    Complete TOP Full Stack Javascript/ Javascript / ES6 Modules

--- fullstack_javascript/practice-space/script.js
+++ fullstack_javascript/practice-space/script.js
@@ -1,22 +1,3 @@
-class Cat {
-    constructor(name) {
-        this.name = name;
-    }
-
-    speak() {
-        return `${this.name} makes a noise.`;
-    }
-}
-
-class Lion extends Cat {
-    speak() {
-        return { test: super.speak(), tast: `${this.name} roars.` };
-    }
-}
-
-let l = new Lion('Fuzzy');
-console.log(l.speak());
-
 
 
 
