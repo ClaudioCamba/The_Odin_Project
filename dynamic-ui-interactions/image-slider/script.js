db2b35c7b9e4e7907c1ed9a4b94ed1e61326eb8c
Claudio Camba <claudio.a.jose@gmail.com>
2022-07-27 15:55:36 +0100

    Add navigation buttons, function to highlight active nav button

--- dynamic-ui-interactions/image-slider/script.js
+++ dynamic-ui-interactions/image-slider/script.js
@@ -1,4 +1,4 @@
-// Slider component control
+// Slide class
 class Slide {
     constructor(elem, width, start, parentW, position) {
         this.elem = elem;
@@ -6,17 +6,45 @@ class Slide {
         this.start = start;
         this.parentW = parentW;
         this.position = position;
+        this.object = this;
+        this.dot = null;
     }
 
-    getStart() { start };
+    btnDot() {
+        const btn = document.createElement('button');
+        btn.innerText = this.position;
+        btn.setAttribute('data-pos', this.position);
+
+        btn.addEventListener('click', () => {
+            sliderComponent.sliderPos(this.start);
+            sliderComponent.activeSlide(this.object);
+        });
+
+        this.dot = btn;
+        return btn;
+    }
+
+    active() {
+        this.dot.classList.add('active');
+        this.elem.classList.add('active');
+    }
+
+    inactive() {
+        this.dot.classList.remove('active');
+        this.elem.classList.remove('active');
+    }
+
 };
 
-const sliderComponent = () => {
+// Slider module
+const sliderComponent = (() => {
+    // Variables
     let slides = [];
     const imgSlider = document.querySelector('.image-slider'); // Scroll position check
     const sliderWrap = document.querySelector('.slider-wrap'); // Width = all images width
     const btnPrev = document.querySelector('.s-prev');
     const btnNext = document.querySelector('.s-next');
+    const dotsWrap = document.querySelector('.nav-dots');
 
     // Create classes
     const createSlides = () => {
@@ -32,43 +60,69 @@ const sliderComponent = () => {
                     startPoint += slide.width;
                 }
             }
+            imgs[i].setAttribute('data-pos', i);
             slides.push(new Slide(imgs[i], slideVal.width, startPoint, wrapElem.width, i));
         };
         console.log(slides);
     };
 
-    // Prev / Next Functions
+    // Next Functions
     const nextSlide = () => {
         for (let i = 0; i < slides.length; i++) {
             if (slides[i].start > imgSlider.scrollLeft) {
-                imgSlider.scroll({ left: slides[i].start, top: 0, behavior: 'smooth' });
+                sliderPos(slides[i].start);
+                activeSlide(slides[i]);
                 break;
             } else if (slides[slides.length - 1].start === imgSlider.scrollLeft) {
-                imgSlider.scroll({ left: 0, top: 0, behavior: 'smooth' });
+                sliderPos(0);
             }
         }
-
     }
 
-
+    // Prev Function
     const prevSlide = () => {
         for (let i = slides.length - 1; i >= 0; i--) {
             if (slides[i].start < imgSlider.scrollLeft) {
-                imgSlider.scroll({ left: slides[i].start, top: 0, behavior: 'smooth' });
+                sliderPos(slides[i].start);
+                activeSlide(slides[i]);
                 break;
             }
         }
     }
 
-    btnPrev.addEventListener('click', prevSlide);
-    btnNext.addEventListener('click', function () {
-        nextSlide()
-        console.log(slides);
-        // updateSlides()
-    });
+    // Navigation dots
+    const navDots = () => {
+        dotsWrap.innerHTML = '';
+        for (const slide of slides) {
+            const wrapLI = document.createElement('li');
+            wrapLI.appendChild(slide.btnDot());
+            dotsWrap.append(wrapLI);
+        }
+    }
 
-    createSlides();
-};
+    // Control slider position
+    const sliderPos = (p) => {
+        return imgSlider.scroll({ left: p, top: 0, behavior: 'smooth' });
+    };
+
+    // Active dot
+    const activeSlide = (e) => {
+        for (const slide of slides) {
+            if (e === slide) {
+                slide.active();
+            } else {
+                slide.inactive();
+            }
+        }
+    }
+
+    return { createSlides, nextSlide, prevSlide, navDots, sliderPos, activeSlide, btnPrev, btnNext }
+})();
 
 // Run main function after page has fully loaded
-window.addEventListener('load', sliderComponent);
+window.addEventListener('load', () => {
+    sliderComponent.createSlides();
+    sliderComponent.navDots();
+    sliderComponent.btnPrev.addEventListener('click', () => sliderComponent.prevSlide());
+    sliderComponent.btnNext.addEventListener('click', () => sliderComponent.nextSlide());
+});
